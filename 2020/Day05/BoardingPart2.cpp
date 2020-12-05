224036367ddf63ed21d3d202fa7331285e5b5617
= <axenth12@gmail.com>
2020-12-05 07:01:00 +0100

    05/12/2020

---
+++ 2020/Day05/BoardingPart2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+static int rowValueLower(float lower, float upper)
+{
+    return((int)(floor((upper - lower)/ 2) + 1));
+}
+
+static int rowValueUpper(float lower, float upper)
+{
+    return((int)(ceil((upper - lower)/ 2)));
+}
+
+int main(void)
+{
+    std::size_t size = 867;
+    int         ID[867];
+    int         result = 0;
+    std::ifstream file("input");
+    if (file.is_open())
+    {
+        std::string line;
+        for(int i = 0 ; i < size; i++)
+        {
+            std::getline(file, line);
+            int upper = 127;
+            int lower = 0;
+            int rowRes = 0;
+            int columnRes = 0;
+
+            for (int j = 0; j < 6; j++)
+            {
+                if (line.c_str()[j] == 'F')
+                    upper -= rowValueLower(lower, upper);
+                if (line.c_str()[j] == 'B')
+                    lower += rowValueUpper(lower, upper);
+            }
+            if (line.c_str()[6] == 'F')
+                rowRes = lower;
+            else if (line.c_str()[6] == 'B')
+                rowRes = upper;
+            
+            lower = 0;
+            upper = 7;
+            for (int j = 7; j < 9; j++)
+            {
+                if (line.c_str()[j] == 'L')
+                    upper -= rowValueLower(lower, upper);
+                if (line.c_str()[j] == 'R')
+                    lower += rowValueUpper(lower, upper);    
+                
+            }
+            if (line.c_str()[9] == 'L')
+                columnRes = lower;
+            else if (line.c_str()[9] == 'R')
+                columnRes = upper;
+            
+            int seatID = rowRes * 8 + columnRes;
+            ID[i] = seatID;
+        }
+    }
+    file.close();
+
+    for(int i = 0 ; i < size; i++)
+    {
+      //  std::cout << ID[i] << std::endl;
+        if (ID[i] > result)
+            result = ID[i];
+    }
+
+    std::cout << "Found: " << result << std::endl;
+    for (int i = 1 ; i < size; i++)
+    {
+        if (ID[i] < ID[i - 1])
+        {
+            int temp = ID[i];
+            ID[i] = ID[i - 1];
+            ID[i - 1] = temp;
+            i = 1;
+        }
+    }
+    for(int i = 0 ; i < size; i++)
+        {
+            if (ID[i + 1] - ID[i] > 1)
+            std::cout << ID[i] + 1 << std::endl;
+        }
+}
+
+
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent Of Code
 
-Nothing really special to be found here. I just like participating in these whenever I have the time for fun but I usually just submit a few of them because of other projects consuming my time.
+Nothing really special to be found here.<br> I just like participating in these whenever I have the time for fun but I usually just submit a few of them because of other projects consuming my time.
+<br><br>
+## Golden Stars
+2019 - 8* (code from day03 onward got lost unfortunatly)<br>
+2020 - ongoing (01/12/2020 - 25/12/2020)
