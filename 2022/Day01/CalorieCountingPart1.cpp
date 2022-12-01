58562056482362cf3f4679084db772822be337a3
jlensing <Axenth@users.noreply.github.com>
2022-12-01 11:35:44 +0100

    2022/Day01

---
+++ 2022/Day01/CalorieCountingPart1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "Usage: CalorieCounting <input_file>" << std::endl;
+		return 1;
+	}
+	FILE *fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		std::cout << "Error: could not open file " << argv[1] << std::endl;
+		return 1;
+	}
+
+	std::vector<int> results;
+
+	int num_lines = 0;
+	int num_read = 0;
+	while (fscanf(fp, "%d", &num_read) != EOF)
+		num_lines++;
+	fclose(fp);
+	fp = fopen(argv[1], "r");
+	int readValue = 0;
+	int total = 0;
+	char *line;
+	size_t length;
+
+	while (getline(&line, &length, fp) != -1)
+	{
+		readValue = 0;
+		readValue = atoi(line);
+		std::cout << line << std::endl;
+		if (readValue == 0)
+		{
+			results.push_back(total);
+			total = 0;
+			readValue = 0;
+		}
+		else
+		{
+			total += readValue;
+		}
+	}
+	int currValue = -1;
+	for (std::vector<int>::iterator it = results.begin(); it != results.end(); it++)
+	{
+		if (*it > currValue)
+			currValue = *it;
+	}
+
+	std::cout << "highest value: " << currValue << std::endl;
+	fclose(fp);
+
+	return 0;
+}
